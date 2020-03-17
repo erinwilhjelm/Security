cac6bab76cc8841e38b26a089c57779154f584e8
Erin <erin.wilhjelm@gmail.com>
2020-03-16 21:18:06 -0400

    database setup

--- server/models/User.js
+++ server/models/User.js
@@ -18,13 +18,7 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-
     }
-
 })
 
 module.exports = User = mongoose.model('users', UserSchema)
